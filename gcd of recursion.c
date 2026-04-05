6ed790706d8bfc514c7c56c9199eb69c2ea7e536
surendramajji94-source <surendramajji94@gmail.com>
2026-04-05 22:19:20 +0530

    Create gcd of recursion.c

---
+++ gcd of recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int gcd(int a, int b)
+{
+if(b == 0)
+    return a;
+else
+    return gcd(b, a % b);
+}
+int main()
+{
+int num1, num2, result;
+printf("Enter two numbers: ");
+scanf("%d %d", &num1, &num2);
+ 
+result = gcd(num1, num2);
+printf("GCD of %d and %d = %d", num1, num2, result);
+return 0;
+}
