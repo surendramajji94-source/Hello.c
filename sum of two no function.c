f25802a98c856417a140c0df26f372de0b9056fe
surendramajji94-source <surendramajji94@gmail.com>
2026-04-05 22:14:46 +0530

    Update sum of two no function.c

--- sum of two no function.c
+++ sum of two no function.c
@@ -1,1 +1,12 @@
-
+ #include <stdio.h>
+int sum(int a, int b) { 
+return a + b;
+} int main()
+ { 
+int x, y, result;
+ printf("Enter two numbers: "); 
+scanf("%d %d", &x, &y);
+ result = sum(x, y);
+ printf("Sum = %d", result); 
+return 0;
+ } 
